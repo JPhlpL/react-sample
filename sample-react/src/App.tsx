e22ebddb41b8b8f48ed4666711fecff1df7d8153
John Philip Lominoque <johnphilipomamalin1206@gmail.com>
2024-05-26 20:01:08 +0800

    Initial - Working on ListGroup and Handling Events

---
+++ sample-react/src/App.tsx
@@ -0,0 +1,15 @@
+import Message from "./Message";
+import ListGroup from "./components/ListGroup";
+
+function App() {
+  return (
+    <div>
+      {/* <Message>
+            </Message> */}
+      {/*  */}
+      <ListGroup></ListGroup>
+    </div>
+  );
+}
+
+export default App;
